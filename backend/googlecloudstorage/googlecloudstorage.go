8ac79074786ef47424f38b3d8a7a33e5abb4042f
Karol Kokoszka <karol.kokoszka@scylladb.com>
2023-02-03 18:48:47 +0100

    smpatch: play with chunksize (set to 256kB)

--- backend/googlecloudstorage/googlecloudstorage.go
+++ backend/googlecloudstorage/googlecloudstorage.go
@@ -1106,7 +1106,7 @@ func (o *Object) Update(ctx context.Context, in io.Reader, src fs.ObjectInfo, op
 	}
 	var newObject *storage.Object
 	err = o.fs.pacer.CallNoRetry(func() (bool, error) {
-		insertObject := o.fs.svc.Objects.Insert(bucket, &object).Media(in, googleapi.ContentType(""), googleapi.ChunkSize(0)).Name(object.Name)
+		insertObject := o.fs.svc.Objects.Insert(bucket, &object).Media(in, googleapi.ContentType(""), googleapi.ChunkSize(256)).Name(object.Name)
 		if !o.fs.opt.BucketPolicyOnly {
 			insertObject.PredefinedAcl(o.fs.opt.ObjectACL)
 		}
